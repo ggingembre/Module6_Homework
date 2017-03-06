f4bb837d33184b3a87c48b3b9c5ddb086e03efed
ggingembre@gmail.com <Sailing2010>
2017-03-06 15:08:47 +0200

    Commit 1: homework 6 new, tasks 1 to 4

--- Tasks4_5_6/User.java
+++ Tasks4_5_6/User.java
@@ -3,7 +3,7 @@ package Tasks4_5_6;
 /**
  * Created by Guillaume Gingembre on 06/03/2017.
  */
-public class User {
+public final class User { // class is immutable, so made it final
 
     private long id;
     private String firstName;
@@ -23,43 +23,45 @@ public class User {
 
     // get and set methods
 
+    // class immutable, so no set methods
+
     public long getId() {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+    ////public void setId(long id) {
+    //    this.id = id;
+    //}
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    //public void setFirstName(String firstName) {
+    //    this.firstName = firstName;
+    //}
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    //public void setLastName(String lastName) {
+    //    this.lastName = lastName;
+    //}
 
     public int getSalary() {
         return salary;
     }
 
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
+    //public void setSalary(int salary) {
+    //    this.salary = salary;
+    //}
 
     public int getBalance() {
         return balance;
     }
 
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
+    //public void setBalance(int balance) {
+    //    this.balance = balance;
+    //}
 }
